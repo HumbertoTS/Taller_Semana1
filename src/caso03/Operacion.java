840c99ff30eaa605f06f2540c991400dd04bbc40
Jerdu03 <JEduardo.031210@gmail.com>
2026-05-27 23:06:06 -0500

    Commit

---
+++ src/caso03/Operacion.java
@@ -0,0 +1,29 @@
+package caso03;
+
+public class Operacion {
+
+    //Metodo que suma con dos enteros
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public double sumar (double a, double b) {
+        return a + b;
+    }
+
+    public int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] arg) {
+
+        //Creación de Objeto
+        Operacion ope = new Operacion();
+
+        //Mostrar los resultados
+
+        System.out.println("Suma de enteros: " + ope.sumar(5, 3));
+        System.out.println("Suma de decimales: " + ope.sumar(5.2, 6.3));
+        System.out.println("Suma de tres enteros: " + ope.sumar(9, 5, 8));
+    }
+}
